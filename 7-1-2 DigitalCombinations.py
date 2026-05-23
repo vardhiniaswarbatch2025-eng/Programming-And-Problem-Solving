51c3eb1f50d002f6d7c6000dd867c260cfc3319e
vardhiniaswarbatch2025-eng <vardhini.aswar.batch2025@sitnagpur.siu.edu.in>
2026-05-23 15:17:48 +0530

    Add files via upload

---
+++ 7-1-2 DigitalCombinations.py
@@ -0,0 +1,9 @@
+# Type Content here...
+a,b,c=input().split()
+digits=[a,b,c]
+
+for i in range(3):
+	for j in range(3):
+		for k in range(3):
+			if i!=j and j!=k and i!=k:
+				print(digits[i]+digits[j]+digits[k])
